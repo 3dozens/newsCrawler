c9210e8daac5778a065c68c244d02c877896e9f6
kento_hayasaka <thunders.1028@gmail.com>
2017-04-17 23:49:59 +0900

    implement GoogleSearchEngine#search

--- newsClawler/src/main/java/entity/impl/GoogleSearchEngine.java
+++ newsClawler/src/main/java/entity/impl/GoogleSearchEngine.java
@@ -9,12 +9,16 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import com.google.gson.Gson;
 
-public class GoogleSearchEngine {
+import entity.impl.GoogleSearchResult.Item;
+import entity.intrfc.SearchEngine;
+
+public class GoogleSearchEngine implements SearchEngine {
 	
 	private final String API_KEY;
 	private final String CUSTOM_SEARCH_ENGINE_ID;
@@ -31,31 +35,48 @@ public class GoogleSearchEngine {
 	 */
 	public List<String> search(String query) {
 
-		URL url = null; 
-		try {
-			url = new URL("https://www.googleapis.com/customsearch/v1?"
-					+ "key=" + API_KEY
-					+ "&cx=" + CUSTOM_SEARCH_ENGINE_ID
-					+ "&q="  + URLEncoder.encode(query, "UTF-8"));
-		} catch (MalformedURLException e) { // TODO: 例外をどこで処理するのがベストか?
-			e.printStackTrace();
-		} catch (UnsupportedEncodingException e) {
-			e.printStackTrace();
+		List<GoogleSearchResult> resultList = new ArrayList<>();
+		int startIndex = 1;
+		for (int i = 0; i < 10; i++) {
+			// TODO: ページ数の調整
+			// 1ループ = 1ページ分のリクエスト
+			
+			// URLの組み立て
+			URL url = null; 
+			try {
+				url = new URL("https://www.googleapis.com/customsearch/v1?"
+						+ "key=" + API_KEY
+						+ "&cx=" + CUSTOM_SEARCH_ENGINE_ID
+						+ "&start=" + startIndex
+						+ "&q="  + URLEncoder.encode(query, "UTF-8"));
+			} catch (MalformedURLException e) { // TODO: 例外をどこで処理するのがベストか?
+				e.printStackTrace();
+			} catch (UnsupportedEncodingException e) {
+				e.printStackTrace();
+			}
+
+			// レスポンスを受け取るReaderの組み立て
+			Reader reader = null;
+			try {
+				reader = new BufferedReader(new InputStreamReader(url.openStream(), StandardCharsets.UTF_8));
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+
+			// APIからレスポンスを受ける
+			// JSONからオブジェクトにマップ
+			// ひとつのGoogleSearchResult = 1ページ分のレスポンス
+			GoogleSearchResult result = new Gson().fromJson(reader, GoogleSearchResult.class);
+			
+			startIndex = result.getQueries().getNextPage().getStartIndex();
+			resultList.add(result);
 		}
 		
-		Reader reader = null;
-		try {
-			reader = new BufferedReader(new InputStreamReader(url.openStream(), StandardCharsets.UTF_8));
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		
-		GoogleSearchResult result = new Gson().fromJson(reader, GoogleSearchResult.class);
-		
-		List<String> urls = result.getItems().stream() // TODO: 検索ページ数を増やす
-												.map(Item::getLink)
-												.collect(Collectors.toList());
-		
+		List<String> urls = resultList.stream()
+										.flatMap(result -> result.getItems().stream())
+										.map(Item::getLink)
+										.collect(Collectors.toList());
+
 		return urls;
 	}
 }
