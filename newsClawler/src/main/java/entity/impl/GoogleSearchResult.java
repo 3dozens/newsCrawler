6c5fd3b6809cadd7f02c8f4dd5fda99b48e56cbd
kento_hayasaka <thunders.1028@gmail.com>
2017-04-17 23:46:57 +0900

    implement GoogleSearchResult

--- newsClawler/src/main/java/entity/impl/GoogleSearchResult.java
+++ newsClawler/src/main/java/entity/impl/GoogleSearchResult.java
@@ -1,9 +1,16 @@
 package entity.impl;
 
+import java.util.Arrays;
 import java.util.List;
 
+/**
+ * Google Custom Search API が返すJSONと対応した構造のクラス
+ * @author Kento Hayasaka thunders.1028@gmail.com
+ *
+ */
 public class GoogleSearchResult {
 	private List<Item> items;
+	private Queries queries;
 
 	public List<Item> getItems() {
 		return items;
@@ -11,5 +18,48 @@ public class GoogleSearchResult {
 
 	public void setItems(List<Item> items) {
 		this.items = items;
+	}
+
+	public Queries getQueries() {
+		return queries;
+	}
+
+	public void setQueries(Queries queries) {
+		this.queries = queries;
+	}
+
+	static public class Item {
+		private String link;
+
+		public String getLink() {
+			return link;
+		}
+
+		public void setLink(String link) {
+			this.link = link;
+		}
+	}
+	
+	static public class Queries {
+		private List<NextPage> nextPage; // Search APIから、要素がひとつの配列の形のJSONで来る
+
+		public NextPage getNextPage() {
+			return nextPage.get(0); // nextPageは必ず要素がひとつのリストなので、その一件を取り出す
+		}
+
+		public void setNextPage(NextPage nextPage) {
+			this.nextPage = Arrays.asList(nextPage);
+		}
+	}
+	static public class NextPage {
+		private int startIndex;
+
+		public int getStartIndex() {
+			return startIndex;
+		}
+
+		public void setStartIndex(int startIndex) {
+			this.startIndex = startIndex;
+		}
 	}
 }
