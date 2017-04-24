4f3f35b7daa3c88e548963b99eced2ddea0011b9
kento_hayasaka <thunders.1028@gmail.com>
2017-04-24 23:21:56 +0900

    Add Crawler comment

--- newsClawler/src/main/java/entity/intrfc/Crawler.java
+++ newsClawler/src/main/java/entity/intrfc/Crawler.java
@@ -4,6 +4,11 @@ import java.util.List;
 
 public interface Crawler {
 
+	/**
+	 * queryで検索し、該当する記事すべての本文を取得する
+	 * @param query 検索する語
+	 * @return 記事の本文
+	 */
 	public List<String> searchArticles(String query);
 	
 }
