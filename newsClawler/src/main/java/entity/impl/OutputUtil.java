a1b1ef44035b1a4df9524ad4d7955d6dcda1513b
kento_hayasaka <thunders.1028@gmail.com>
2017-05-04 00:04:25 +0900

    Modify OutputUtil to deal with Map

--- newsClawler/src/main/java/entity/impl/OutputUtil.java
+++ newsClawler/src/main/java/entity/impl/OutputUtil.java
@@ -7,6 +7,7 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
 import java.util.List;
+import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.Stream;
@@ -18,11 +19,17 @@ public class OutputUtil {
 	/**
 	 * パースした文章をファイルに出力します
 	 * 一つの文章ごとに、ファイルを連番で出力します
-	 * @param parsed パースした文章
+	 * @param parsed パースした文章のマップ
 	 * @param filePath ファイルパス(このパスのファイル名の後ろに連番がついたファイルが出力される) 
 	 *                    例: 入力ファイル名:sports -> 実際のファイル名:sports_1, sports_2...
 	 */
-	public static void output(List<List<String>> parsed, Path filePath) {
+	public static void output(Map<String, List<String>> parsed, Path filePath) {
+		try {
+			Files.createDirectories(filePath);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
 		OutputUtil.output(parsed, filePath, OutputUtil.findMaxIndex(filePath) + 1);
 	}
 
@@ -33,15 +40,43 @@ public class OutputUtil {
 	 * @param filePath 出力するファイルパス
 	 * @param startIndex 連番のスタート
 	 */
-	private static void output(List<List<String>> parsed, Path filePath, int startIndex) {
+	private static void output(Map<String, List<String>> parsed, Path filePath, int startIndex) {
+		Path urlPath = Paths.get(filePath.toString() + "_urlinfo.txt");
+		
 		int i = startIndex;
-		for (List<String> words : parsed) {
+		for (Map.Entry<String, List<String>> entry : parsed.entrySet()) {
 			Path writePath = Paths.get(filePath.toString() + "_" + i++);
-			try {
-				Files.write(writePath, words, Charset.forName("UTF-8"), StandardOpenOption.CREATE);
-			} catch (IOException e) {
-				e.printStackTrace(); // TODO: 例外処理の方法を考える
-			}
+			
+			outputParsedText(writePath, entry.getValue());
+			
+			outputURLInfo(urlPath, entry.getKey(), writePath.getFileName().toString());
+		}
+	}
+
+	/**
+	 * パースした文章をPathに出力します
+	 * @param filePath 出力先Path
+	 * @param parsed パースした文章
+	 */
+	private static void outputParsedText(Path filePath, List<String> parsed) {
+		try {
+			Files.write(filePath, parsed, Charset.forName("UTF-8"), StandardOpenOption.CREATE);
+		} catch (IOException e) {
+			e.printStackTrace(); // TODO: 例外処理の方法を考える
+		}
+	}
+	
+	/**
+	 * 取得した記事のURL情報をPathに出力します
+	 * @param filePath 出力先Path
+	 * @param url 記事のURL
+	 * @param fileName 記事を出力したファイル名
+	 */
+	private static void outputURLInfo(Path filePath, String url, String fileName) {
+		try {
+			Files.write(filePath, (fileName + ":" + url + "\n").getBytes(), StandardOpenOption.CREATE, StandardOpenOption.APPEND);
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
 	}
 	
